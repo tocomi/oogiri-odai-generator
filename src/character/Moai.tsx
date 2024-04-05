93282374e8a8e9cc9dc644e3f665cf8fdea30831
tocomi <tocomi0112@gmail.com>
2024-04-06 00:55:16 +0900

    feat: moai animation

--- src/character/Moai.tsx
+++ src/character/Moai.tsx
@@ -23,11 +23,11 @@ export const Moai: FC = () => {
   }, [message]);
 
   return (
-    <div className="fixed flex items-center bottom-4 right-0 text-9xl">
+    <div className="fixed flex items-center bottom-4 right-2 text-9xl">
       <span className="inline-block bg-white border-cyan-900 shadow-lg border-2 rounded-2xl mr-[-48px] p-4 text-2xl font-bold h-36 w-96">
         {displayedMessage}
       </span>
-      <span className="text-9xl">🗿</span>
+      <span className="text-9xl animate-sway">🗿</span>
     </div>
   );
 };
