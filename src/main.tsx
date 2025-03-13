55f8241e22532254f308c094ea952d6c704296c7
tocomi <tocomi0112@gmail.com>
2025-03-14 01:34:16 +0900

    chore: add react scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
+import { scan } from "react-scan";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+
+scan({
+  enabled: true,
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,21 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+// const ReactCompilerConfig = {
+//   target: '19'
+// };
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      // babel: {
+      //   plugins: [
+      //     ["babel-plugin-react-compiler", ReactCompilerConfig],
+      //   ],
+      // },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
