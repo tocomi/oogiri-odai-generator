aaddbdf83d92b350a8704cdb260bd4630992026e
tocomi <tocomi0112@gmail.com>
2025-03-14 03:22:39 +0900

    feat: apply react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
 import path from 'path'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// const ReactCompilerConfig = {
-//   target: '19'
-// };
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
-      // babel: {
-      //   plugins: [
-      //     ["babel-plugin-react-compiler", ReactCompilerConfig],
-      //   ],
-      // },
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
     }),
   ],
   resolve: {
